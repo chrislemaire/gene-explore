0a748d34fef44ed72f94456622b109e4f5770892
Chris Lemaire <c.lemaire@student.tudelft.nl>
2018-06-23 19:53:45 +0200

    Add list of paths to cache files

--- core/src/main/scala/com/clemaire/genominator/core/gfa/reference/io/SimpleReferenceWriter.scala
+++ core/src/main/scala/com/clemaire/genominator/core/gfa/reference/io/SimpleReferenceWriter.scala
@@ -2,7 +2,7 @@ package com.clemaire.genominator.core.gfa.reference.io
 
 import com.clemaire.genominator.core.gfa.reference.ReferenceNode
 
-class SimpleReferenceWriter {
+class SimpleReferenceWriter() {
 
   def write(referenceNode: ReferenceNode): Unit = {
 
