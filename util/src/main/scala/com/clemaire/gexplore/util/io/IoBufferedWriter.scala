032a569d0807dd9c738d19341a29a5a0c0d70f04
Chris Lemaire <c.lemaire@student.tudelft.nl>
2019-01-15 15:56:57 +0100

    Add mutable path and buffer size to IoBufferedWriter

--- util/src/main/scala/com/clemaire/gexplore/util/io/IoBufferedWriter.scala
+++ util/src/main/scala/com/clemaire/gexplore/util/io/IoBufferedWriter.scala
@@ -6,6 +6,16 @@ import java.nio.file.{Files, Path, StandardOpenOption}
 trait IoBufferedWriter extends Writer {
 
   /**
+    * The [[Path]] location of the file to write to.
+    */
+  private[this] var _path: Path = _
+
+  /**
+    * The size of the buffer within this Writer's [[DataOutputStream]].
+    */
+  private[this] var _size: Int = 8192
+
+  /**
     * The [[DataOutputStream]] to write data to.
     */
   private[this] var _os: DataOutputStream = _
@@ -17,19 +27,39 @@ trait IoBufferedWriter extends Writer {
   protected[this] def os: DataOutputStream = _os
 
   /**
-    * Sets the path to the file this [[IoBufferedWriter]]
-    * should write to.
-    *
-    * @param path The [[Path]] of the file to write to.
+    * Reopens the output stream after closing any existing OS.
+    * @return This [[IoBufferedWriter]]
     */
-  protected[this] def withPath(path: Path): this.type = {
+  private[this] def reopenOS(): this.type = {
     if (_os != null) {
       _os.close()
     }
     _os = new DataOutputStream(new BufferedOutputStream(
-      Files.newOutputStream(path,
-        StandardOpenOption.WRITE, StandardOpenOption.CREATE)))
+      Files.newOutputStream(_path,
+        StandardOpenOption.WRITE, StandardOpenOption.CREATE), _size))
     this
+  }
+
+  /**
+    * Sets the path to the file this [[IoBufferedWriter]]
+    * should write to.
+    *
+    * @param path The [[Path]] of the file to write to.
+    * @return This [[IoBufferedWriter]].
+    */
+  protected[this] def withPath(path: Path): this.type = {
+    _path = path
+    reopenOS()
+  }
+
+  /**
+    * Sets the buffer-size internal to this [[IoBufferedWriter]].
+    * @param size The desired size of the buffer.
+    * @return This [[IoBufferedWriter]]
+    */
+  protected[this] def withBufferSize(size: Int): this.type = {
+    _size = size
+    reopenOS()
   }
 
   override def flush(): Unit =
