143aacb24e09d08dff7a22ad423b7ba1ba8d1520
Chris Lemaire <c.lemaire@student.tudelft.nl>
2019-01-16 13:29:10 +0100

    Implement SRIndexDataReader reading methods

--- core/src/main/scala/com/clemaire/gexplore/core/gfa/reference/reading/index/SRIndexDataReader.scala
+++ core/src/main/scala/com/clemaire/gexplore/core/gfa/reference/reading/index/SRIndexDataReader.scala
@@ -1,5 +1,34 @@
 package com.clemaire.gexplore.core.gfa.reference.reading.index
 
-class SRIndexDataReader {
+import java.io.DataInputStream
+import java.nio.ByteBuffer
+
+import com.clemaire.gexplore.core.gfa.DataReader
+import com.clemaire.gexplore.core.gfa.reference.index.ReferenceChunkIndex
+import com.lodborg.intervaltree.IntegerInterval
+import com.lodborg.intervaltree.Interval.Bounded
+
+trait SRIndexDataReader
+  extends DataReader[ReferenceChunkIndex] {
+
+  override protected def read(is: DataInputStream)
+  : ReferenceChunkIndex =
+    ReferenceChunkIndex(
+      is.readInt(),
+      is.readLong(),
+      is.readInt(),
+      new IntegerInterval(is.readInt, is.readInt, Bounded.CLOSED),
+      new IntegerInterval(is.readInt, is.readInt, Bounded.CLOSED)
+    )
+
+  override protected def read(ib: ByteBuffer)
+  : ReferenceChunkIndex =
+    ReferenceChunkIndex(
+      ib.getInt,
+      ib.getLong,
+      ib.getInt,
+      new IntegerInterval(ib.getInt, ib.getInt, Bounded.CLOSED),
+      new IntegerInterval(ib.getInt, ib.getInt, Bounded.CLOSED)
+    )
 
 }
