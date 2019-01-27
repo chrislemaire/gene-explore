e91d9aff9528d2062012340663336a4500b7cabb
Chris Lemaire <c.lemaire@student.tudelft.nl>
2019-01-27 19:05:16 +0100

    Move Structural node into reference package

--- core/src/main/scala/com/clemaire/gexplore/core/gfa/data/StructuralNode.scala
+++
@@ -1,8 +0,0 @@
-package com.clemaire.gexplore.core.gfa.data
-
-case class StructuralNode(id: Int,
-                          layer: Int,
-                          fileOffset: Long,
-                          contentLength: Int,
-                          incomingEdges: Traversable[(Int, Long)],
-                          outgoingEdges: Traversable[(Int, Long)])
---
+++ core/src/main/scala/com/clemaire/gexplore/core/gfa/reference/data/StructuralNode.scala
@@ -0,0 +1,7 @@
+package com.clemaire.gexplore.core.gfa.reference.data
+
+case class StructuralNode(id: Int,
+                          layer: Int,
+                          contentLength: Int,
+                          incomingEdges: Set[Int],
+                          outgoingEdges: Set[Int])
