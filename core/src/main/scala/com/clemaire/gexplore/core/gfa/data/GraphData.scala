68b9a6f099f00ea3a89819a33baeb468103d6d36
Chris Lemaire <c.lemaire@student.tudelft.nl>
2019-01-28 22:32:03 +0100

    Add path list to graph data

--- core/src/main/scala/com/clemaire/gexplore/core/gfa/data/GraphData.scala
+++ core/src/main/scala/com/clemaire/gexplore/core/gfa/data/GraphData.scala
@@ -1,8 +1,10 @@
 package com.clemaire.gexplore.core.gfa.data
 
 import com.clemaire.gexplore.core.gfa.reference.index.{GCIndex, NodeIndex}
+import com.clemaire.gexplore.core.gfa.CachePathList
 
-case class GraphData(referenceIndex: NodeIndex,
+case class GraphData(paths: CachePathList,
+                     referenceIndex: NodeIndex,
                      coordinateIndex: GCIndex,
                      genomes: Map[Int, String]) {
   val genomeNames: Map[String, Int] = genomes.map(_.swap)
